176c3597cbccfbe1484e583da9082415bd02fd39
fyh pc <aszx365289461@github.com>
2018-05-27 14:00:58 +0800

    add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,28 @@
 module.exports={
-    title:'欢迎来到我的网络日志',
+    title:'奶油面包好好吃啊',
     description:'my blog',
     themeConfig:{
         nav:[
-                {text:'前端',link:'/frontend/'},
-        ]
+            {text:'首页',link:'/'},
+            {text:'日志',link:'/blog/'},
+            {text:'Github',link:'https://github.com/aszx-fyh'},
+        ],
+        sidebar:{
+            '/blog/':[
+                {
+                    title:'文章',
+                    collapsable:false,
+                    children:[
+                        ['frontend/','前端相关'],
+                        ['tools/','工具使用小结']
+                    ]
+                },
+                ['collections','搜集'],
+            ]
+                
+            
+            
+            
+        }
     }
-}
+};
--- docs/blog/collections.md
+++ docs/blog/collections.md
@@ -1,3 +1,3 @@
-# 收集
+# 搜集
 + [markdown语法](http://www.markdown.cn "markdown")
 + [handlebars模版引擎官网](http://handlebarsjs.com)
